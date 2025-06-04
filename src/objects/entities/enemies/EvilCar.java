1bf0b2f833632ab39e37ba06af6fb6a6dfcd20ef
iscaiden <caiden.luebehusen@gmail.com>
2025-06-04 16:35:51 -0400

    Evil car with animation

--- src/objects/entities/enemies/EvilCar.java
+++ src/objects/entities/enemies/EvilCar.java
@@ -1,49 +1,57 @@
 package objects.entities.enemies;
 
 import engine.Main;
-import engine.states.Game;
-import objects.GameObject;
 import objects.entities.Entity;
-import objects.platforms.Platform;
-import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
-import org.newdawn.slick.geom.Rectangle;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SpriteSheet;
 import org.newdawn.slick.state.StateBasedGame;
 import ui.Images;
 import values.CarValues;
 import world.Cell;
 
-import java.util.ArrayList;
-
 public class EvilCar extends Entity {
-
+    private Image image;
+    private SpriteSheet mySheet;
+    private Image currentFrame;
+    private int step = 0;
+    private int frames = 0;
+    private int framesPerStep = 6;
     public EvilCar(float x, float y) {
         super(x, y, CarValues.X_SPEED, CarValues.Y_SPEED, CarValues.HEALTH, CarValues.ATTACK, Images.car1, CarValues.IFRAMES);
+        image = Images.car1;
+        w = image.getWidth();
+        h = image.getHeight();
+        mySheet = Images.movingCar;
+        currentFrame = mySheet.getSprite(0,0);
     }
 
     public void render(Graphics g) {
         super.render(g);
-        g.setColor(Color.white);
-        g.draw(getBounds());
+        float renderOffsetY = h - currentFrame.getHeight();
+            currentFrame.draw(x, y + renderOffsetY);
     }
 
     public void update(GameContainer gc, StateBasedGame sbg, int delta) {
-        xVelocity = -xSpeed;
 
-        if (x <= -w) {
-            x = Main.getScreenWidth() + w;
+        xVelocity -= (float) xSpeed / 5;
+
+        if (x <= Cell.getWidth() * 2) {
+            x = Main.getScreenWidth() - (Cell.getWidth() * 2) - w;
+            xVelocity = 0;
         }
+        frames+=2;
+        if(frames % framesPerStep == 0)
+        {
+            step++;
+        }
+        if(step >= mySheet.getHorizontalCount())
+        {
+            step = 0;
+        }
+        currentFrame = mySheet.getSprite(step, 0);
 
-        newX = x + xVelocity;
-        newY = y + yVelocity;
-
-        x = newX;
-        y = newY;
+        super.update(gc, sbg, delta);
     }
-
-    public Rectangle getBounds() {
-        return new Rectangle(x,y + (Cell.getHeight() / 8),w,h - (Cell.getHeight() / 8));
-    }
-
 }
